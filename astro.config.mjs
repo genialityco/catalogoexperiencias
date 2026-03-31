0ff44da0ad4deef8867569f079639b0cf5275a51
Sebastian Cardona Rios <112008261+sebastian010800@users.noreply.github.com>
2026-03-31 14:33:20 -0500

    fix: despliegue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  output: "server",
+  adapter: netlify(),
+  integrations: [react(), tailwind()],
 });
